f60492d8ebc456347b9a0e4a27802d6032a53008
mgtoxd <51752260+mgtoxd@users.noreply.github.com>
2020-01-11 17:15:22 +0800

    2020-1-11 adapter

---
+++ src/pers/mtx/adapter/interfaceadapter/AbsAdapter.java
@@ -0,0 +1,22 @@
+package pers.mtx.adapter.interfaceadapter;
+
+
+//在AbsAdapter 我们将Interface4 中的方法进行默认实现
+public abstract class AbsAdapter implements Interface4 {
+    public void m1()
+    {
+
+    }
+    public void m2()
+    {
+
+    }
+    public void m3()
+    {
+
+    }
+    public void m4()
+    {
+
+    }
+}
---
+++ src/pers/mtx/adapter/interfaceadapter/Interface4.java
@@ -0,0 +1,4 @@
+package pers.mtx.adapter.interfaceadapter;
+
+public interface Interface4 {
+}
